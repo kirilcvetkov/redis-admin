64f3094073cff2ee42e92270c9bc97117b103e79
Kiril Cvetkov <kcvetkov@live.com>
2023-04-29 19:14:22 -0700

    Adding delete in tree items

--- resources/js/Components/TreeItem.vue
+++ resources/js/Components/TreeItem.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
-import { FolderIcon, DocumentIcon, ChevronDownIcon, ChevronRightIcon } from '@heroicons/vue/24/outline'
+import { FolderIcon, DocumentIcon, ChevronDownIcon, ChevronRightIcon, TrashIcon } from '@heroicons/vue/24/outline'
 import TreeList from "@/components/TreeList.vue";
 
 defineProps({
@@ -14,43 +14,62 @@ defineProps({
 </script>
 
 <template>
-  <li>
-    <button
-      v-if="children"
-      type="button"
-      class="text-xs flex items-center text-left w-full text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-      aria-controls="dropdown-pages"
-      data-collapse-toggle="dropdown-pages"
-      @click="show = ! show"
-    >
-      <ChevronRightIcon
-        v-if="! show"
-        class="w-4 h-4 mr-1 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-      />
-      <ChevronDownIcon
-        v-else
-        class="w-4 h-4 mr-1 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-      />
-      <FolderIcon
-        class="w-5 h-5 mr-1 text-blue-400 transition duration-75 dark:text-blue-400 group-hover:text-blue-400 dark:group-hover:text-white"
-      />
-      <span
-        class="flex-1 whitespace-nowrap"
-      >{{ name }}</span>
-    </button>
-
-    <Link
-      v-else
-      :only="['item']"
-      :href="route('show', { selectedConnection: $page.props.selectedConnection, key: itemKey })"
-      class="text-xs flex items-center pl-2 w-full text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-    >
-      <DocumentIcon
+  <li class="whitespace-nowrap">
+    <div class="flex items-center justify-between w-full hover:bg-gray-700">
+      <button
+        v-if="children"
+        type="button"
+        class="text-xs flex items-center text-left text-base font-normal text-white"
+        aria-controls="dropdown-pages"
+        data-collapse-toggle="dropdown-pages"
         @click="show = ! show"
-        class="w-4 h-4 text-yellow-400 transition duration-75 dark:text-yellow-400 group-hover:text-yellow-900 dark:group-hover:text-white mr-1"
-      />
-      <span class="flex-1 text-left whitespace-nowrap">{{ name }} {{ count === -1 ? '' : '(' + count + ')' }}</span>
-    </Link>
+      >
+        <ChevronRightIcon
+          v-if="! show"
+          class="w-4 h-4 mr-1 transition duration-75 text-gray-400"
+        />
+        <ChevronDownIcon
+          v-else
+          class="w-4 h-4 mr-1 transition duration-75 text-gray-400"
+        />
+        <FolderIcon
+          class="w-5 h-5 mr-1 transition duration-75 text-blue-400"
+        />
+        <span class="flex-1 whitespace-nowrap">
+          {{ name }}
+        </span>
+      </button>
+
+      <Link
+        v-else
+        :only="['item']"
+        :href="route('show', { selectedConnection: $page.props.selectedConnection, key: itemKey })"
+        class="text-xs flex items-center pl-2 text-base font-normal text-white"
+      >
+        <DocumentIcon
+          @click="show = ! show"
+          class="w-4 h-4 text-yellow-400 transition duration-75 mr-1"
+        />
+        <span class="flex-1 text-left whitespace-nowrap">
+          {{ name }}
+          <span
+            v-if="count > -1"
+            class="bg-gray-700 text-gray-300 text-xs font-medium ml-2 px-1 py-0.5 rounded">
+            {{ count }}
+          </span>
+        </span>
+      </Link>
+
+      <button
+        type="button"
+        class="text-xs text-base font-normal text-white"
+        aria-controls="dropdown-pages"
+        data-collapse-toggle="dropdown-pages"
+        @click="show = ! show"
+      >
+        <TrashIcon class="w-3 h-3 text-red-500" />
+      </button>
+    </div>
 
     <Transition
       enter-from-class="opacity-0"
